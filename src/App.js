1122a7814e4ce517430e2139a6bd2cb961d63cdd
Дмитрий Чунихин <chunihin-df@yandex.ru>
2021-09-08 22:51:46 +0500

    homework-2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Layout from "./components/Layout";
+import Footer from "./components/Footer";
+import "./index.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World, React.JS!
-        </p>
-      </header>
-    </div>
-  );
-}
+import LayoutBg1 from "./assets/thoth-tarot-the-fool.jpg";
+import LayoutBg2 from "./assets/foolcard3.jpg";
+
+
+const App = () => (
+  <>
+    <Header title="A tarot deck" descr="brief descriptions of the trump cards" />
+    <Layout urlBg={LayoutBg1} title="The Fool" descr="The trump of Crowley tarot deck numbered 0" />
+    <Layout colorBg="red"/>
+    <Layout urlBg={LayoutBg2} title="The Fool" descr="The trump of Marseille tarot deck numbered 0" />
+    <Footer />
+  </>
+);
 
 export default App;
---
+++ src/components/Layout/index.js
@@ -0,0 +1,31 @@
+import style from "./style.module.css"
+
+const Layout = ({ title, descr, urlBg, colorBg }) => {
+    const layoutBg = { backgroundImage: urlBg && `url(${urlBg})`, backgroundColor: colorBg };
+    return (
+        <section className={style.root} style={layoutBg}>
+            <div className={style.wrapper}>
+                <article>
+                    {title &&
+                        (
+                            <div className={style.title}>
+                                <h3>{title}</h3>
+                                <span className="separator"></span>
+                            </div>
+                        )
+                    }
+                    {descr
+                        &&
+                        (
+                            <div className={`${style.desc} ${style.full}`}>
+                                <p>{descr}</p>
+                            </div>
+                        )
+                    }
+                </article>
+            </div>
+        </section>
+    );
+}
+
+export default Layout;
